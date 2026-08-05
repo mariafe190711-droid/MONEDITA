076f8c20e98e7cd5f64c08f3657f8934828f259f
mariafe190711-droid <mariafe190711@gmail.com>
2026-08-05 15:45:23 -0500

    Update codiguito.py
    SE AGREGO CONVERSION A LIBRAS EXTERLINAS
    
    MARIA LEGUIA

--- codiguito.py
+++ codiguito.py
@@ -19,4 +19,30 @@ elif opcion == "2":
     print("Equivale a", euros, "euros.")
 
 else:
+    print("Opción no válida.")# ==========================================
+# CONVERSOR DE MONEDAS - VERSIÓN 3
+# Autor(a): MARIA LEGUIA
+# ==========================================
+
+soles = float(input("Ingrese la cantidad en soles: "))
+
+print("1. Dólares")
+print("2. Euros")
+print("3. Libras Esterlinas")
+
+opcion = input("Seleccione una opción: ")
+
+if opcion == "1":
+    dolares = soles / 3.60
+    print("Equivale a", dolares, "dólares.")
+
+elif opcion == "2":
+    euros = soles / 4.20
+    print("Equivale a", euros, "euros.")
+
+elif opcion == "3":
+    libras = soles / 4.80
+    print("Equivale a", libras, "libras.")
+
+else:
     print("Opción no válida.")
