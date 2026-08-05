6b9a4194d127f73553da403dd62f10585d712caa
mariafe190711-droid <mariafe190711@gmail.com>
2026-08-05 15:50:50 -0500

    Update codiguito.py
    AGREGADA LA CONVERSION A YENES
    
    MARIA LEGUIA

--- codiguito.py
+++ codiguito.py
@@ -1,26 +1,5 @@
 # ==========================================
-# CONVERSOR DE MONEDAS - VERSIÓN 2
-# Autor(a): MARIA LEGUIA
-# ==========================================
-
-soles = float(input("Ingrese la cantidad en soles: "))
-
-print("1. Dólares")
-print("2. Euros")
-
-opcion = input("Seleccione una opción: ")
-
-if opcion == "1":
-    dolares = soles / 3.60
-    print("Equivale a", dolares, "dólares.")
-
-elif opcion == "2":
-    euros = soles / 4.20
-    print("Equivale a", euros, "euros.")
-
-else:
-    print("Opción no válida.")# ==========================================
-# CONVERSOR DE MONEDAS - VERSIÓN 3
+# CONVERSOR DE MONEDAS - VERSIÓN 4
 # Autor(a): MARIA LEGUIA
 # ==========================================
 
@@ -29,6 +8,7 @@ soles = float(input("Ingrese la cantidad en soles: "))
 print("1. Dólares")
 print("2. Euros")
 print("3. Libras Esterlinas")
+print("4. Yenes")
 
 opcion = input("Seleccione una opción: ")
 
@@ -43,6 +23,10 @@ elif opcion == "2":
 elif opcion == "3":
     libras = soles / 4.80
     print("Equivale a", libras, "libras.")
+
+elif opcion == "4":
+    yenes = soles * 41.5
+    print("Equivale a", yenes, "yenes.")
 
 else:
     print("Opción no válida.")
